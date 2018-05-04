da5b9e99ba34a55da1b5c554b9603c96a8c7de9e
All_Blue <909916737@qq.com>
2018-05-04 22:48:15 +0800

    增加网络请求类

---
+++ app/src/main/java/com/jim/videoplayerdemo/player1/HttpUtils.java
@@ -0,0 +1,36 @@
+package com.jim.videoplayerdemo.player1;
+
+import android.app.DownloadManager;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Jim on 2018/5/4.
+ */
+
+public class HttpUtils {
+
+    public static final int READ_TIME_OUT=60000;
+    public static final int CONNECT_OUT=60000;
+
+    public static URLConnection openConnection(String request) {
+        HttpURLConnection urlConnection=null;
+        try {
+            URL url = new URL(request);
+            urlConnection= (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(READ_TIME_OUT);
+            urlConnection.setConnectTimeout(CONNECT_OUT);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            return urlConnection;
+        }
+    }
+
+}
